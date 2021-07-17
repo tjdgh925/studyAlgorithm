015448033ab2688a3baedfc9fb485f40fe88f03a
장성호 <tjdgh925@gmail.com>
2021-07-17 23:13:53 +0900

    BOJ_4673_셀프넘버

--- implementation/boj_4673_셀프넘버.java
+++ implementation/boj_4673_셀프넘버.java
@@ -6,16 +6,30 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class boj_4673_셀프넘버 {
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+	static boolean check[];
+
+	public static void main(String[] args) throws IOException {
+		check = new boolean[101];
+
+		for (int i = 1; i < 101; i++) {
+			int n = selfNumber(i);
+
+			if (n < 101) {
+				check[n] = true;
+			}
 		}
-		Arrays.sort(arr);
-		for (int i = 0; i < N; i++) {
-			System.out.println(arr[i]);
+		for (int i = 1; i < 101; i++) {
+			if (!check[i])
+				System.out.println(i);
 		}
+	}
+
+	private static int selfNumber(int n) {
+		int sum = n;
+		while (n != 0) {
+			sum = sum + (n % 10);
+			n = n / 10;
+		}
+		return sum;
 	}
 }
