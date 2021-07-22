e0ebf5f81522c07c69e0afe0d9e5e0a421da2f95
장성호 <tjdgh925@gmail.com>
2021-07-22 22:40:23 +0900

    BOJ_11279_최대 힙

---
+++ greedyAlgorithm/java/boj_11279_최대힙.java
@@ -0,0 +1,24 @@
+package greedyAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class boj_11279_최대힙 {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				int result = (!maxHeap.isEmpty()) ? maxHeap.poll() : 0; 
+				System.out.println(result);
+			}
+			else
+				maxHeap.add(x);
+		}
+	}
+}
