26b77bd250c547619df8d61e84f2c7a9cc697c94
장성호 <tjdgh925@gmail.com>
2022-01-01 22:15:57 +0900

    Create boj_11053_가장긴증가하는부분수열 copy.cpp

---
+++ dynamicProgramming/boj_11053_가장긴증가하는부분수열 copy.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1001;
+int dp[MAX], seq[MAX];
+int N, result = 0;
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> seq[i];
+    dp[i] = 1;
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < i; j++) {
+      if (seq[j] < seq[i] && dp[j] >= dp[i])
+        dp[i] = dp[j] + 1;
+    }
+    result = max(result, dp[i]);
+  }
+
+  cout << result << "\n";
+}
