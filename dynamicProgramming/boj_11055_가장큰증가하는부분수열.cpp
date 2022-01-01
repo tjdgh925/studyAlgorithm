7cf667393fa4d6d8862df5c67971a01914e243e5
장성호 <tjdgh925@gmail.com>
2022-01-01 22:16:01 +0900

    Create boj_11055_가장큰증가하는부분수열.cpp

---
+++ dynamicProgramming/boj_11055_가장큰증가하는부분수열.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1001;
+int dp[MAX], seq[MAX];
+int N, result = 0;
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> seq[i];
+    dp[i] = seq[i];
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < i; j++) {
+      if (seq[j] < seq[i])
+        dp[i] = max(dp[i], dp[j] + seq[i]);
+    }
+    result = max(result, dp[i]);
+  }
+
+  cout << result << "\n";
+}
