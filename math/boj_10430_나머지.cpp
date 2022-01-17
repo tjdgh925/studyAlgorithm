13ece23ac638408e1914accd69873426d4f2abe3
장성호 <tjdgh925@gmail.com>
2022-01-17 19:19:06 +0900

    Create boj_10430_나머지.cpp

---
+++ math/boj_10430_나머지.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+int main() {
+  cin >> a >> b >> c;
+  cout << (a + b) % c << "\n";
+  cout << ((a % c) + (b % c)) % c << "\n";
+  cout << (a * b) % c << "\n";
+  cout << ((a % c) * (b % c)) % c << "\n";
+}
