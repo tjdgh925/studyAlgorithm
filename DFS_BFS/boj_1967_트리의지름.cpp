48af601a7461de949132d3a4921b278a250fc8cb
장성호 <tjdgh925@gmail.com>
2022-01-26 22:54:29 +0900

    Create boj_1967_트리의지름.cpp

---
+++ DFS_BFS/boj_1967_트리의지름.cpp
@@ -0,0 +1,52 @@
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 100001;
+int N, maxDist, maxNode;
+;
+
+struct Node {
+  int index;
+  int distance;
+};
+
+bool visit[MAX];
+vector<Node> tree[MAX];
+
+void dfs(int idx, int dist) {
+  if (visit[idx])
+    return;
+
+  if (maxDist < dist) {
+    maxDist = dist;
+    maxNode = idx;
+  }
+  visit[idx] = true;
+
+  for (int i = 0; i < tree[idx].size(); i++) {
+    int next = tree[idx][i].index;
+    int nextDist = tree[idx][i].distance;
+    dfs(next, dist + nextDist);
+  }
+}
+
+int main() {
+  cin >> N;
+
+  for (int i = 0; i < N - 1; i++) {
+    int n, p, d;
+    cin >> n >> p >> d;
+    tree[n].push_back({p, d});
+    tree[p].push_back({n, d});
+  }
+
+  dfs(1, 0);
+  memset(visit, false, sizeof(visit));
+  maxDist = 0;
+  dfs(maxNode, 0);
+
+  cout << maxDist;
+}
