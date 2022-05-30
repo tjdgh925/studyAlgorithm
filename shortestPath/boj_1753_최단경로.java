f911db9774ac9cc5363532cc4797b25f0945c92d
Sungho Jang <tjdgh925@gmail.com>
2022-05-30 19:07:22 +0900

    Update boj_1753_최단경로.java

--- shortestPath/boj_1753_최단경로.java
+++ shortestPath/boj_1753_최단경로.java
@@ -1,95 +1,101 @@
-package shortestPath;
+package graph;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Node4 implements Comparable<Node4> {
-	int index;
-	int distance;
-	
-	public Node4(int index, int distance) {
-		this.index = index;
-		this.distance = distance;
-	}
-	
-	public int getIndex() {
-		return index;
-	}
-	
-	public int getDistance() {
-		return distance;
-	}
-	
-	@Override
-	public int compareTo(Node4 other) {
-		// TODO Auto-generated method stub
-		if (this.distance < other.distance)
-			return -1;
-		return 1;
-	}
-}
 public class boj_1753_최단경로 {
+    static class Node implements Comparable<Node> {
+        private int index;
+        private int distance;
 
-	static int MAX = Integer.MAX_VALUE;
-	static int N, M, K;
-	public static ArrayList<ArrayList<Node4>> graph = new ArrayList<ArrayList<Node4>>();
-	public static int[] d = new int[100001];
-	static StringTokenizer st;
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
 
-	public static void dijkstra(int start) {
-		// TODO Auto-generated method stub
-		PriorityQueue<Node4> pq = new PriorityQueue<Node4>();
-		pq.offer(new Node4(start, 0));
-		d[start] = 0;
-		while(!pq.isEmpty()) {
-			Node4 temp = pq.poll();
-			int dist = temp.getDistance();
-			int now = temp.getIndex();
-			if(d[now] < dist) continue;
-			for (int i = 0; i < graph.get(now).size(); i++) {
-				int cost = d[now] + graph.get(now).get(i).getDistance();
-				if (cost < d[graph.get(now).get(i).getIndex()]) {
-					d[graph.get(now).get(i).getIndex()] = cost;
-					pq.offer(new Node4(graph.get(now).get(i).getIndex(), cost));
-				}	
-			}
-		}
-		
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(br.readLine());
+        public int getIndex() {
+            return index;
+        }
 
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<Node4>());
-		}
+        public void setIndex(int index) {
+            this.index = index;
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			graph.get(a).add(new Node4(b,c));
-		}
-		
-		Arrays.fill(d, MAX);
-		
-		dijkstra(K);
-		
-		for (int i = 1; i <= N; i++) {
-			if(d[i] == Integer.MAX_VALUE)
-				System.out.println("INF");
-			else
-				System.out.println(d[i]);			
-		}
-	}
+        public int getDistance() {
+            return distance;
+        }
 
+        public void setDistance(int distance) {
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.distance < o.distance)
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+    static int V, E, start;
+    static List<List<Node>> graph = new ArrayList<>();
+    static int d[] = new int[20001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(br.readLine());
+        Arrays.fill(d, Integer.MAX_VALUE);
+
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph.get(s).add(new Node(e, c));
+        }
+
+        djikstra(start);
+
+        for (int i = 1; i <= V; i++) {
+            if (d[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else
+                System.out.println(d[i]);
+        }
+    }
+
+    private static void djikstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int index = node.getIndex();
+            int dist = node.getDistance();
+
+            if (d[index] < dist) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(index).size(); i++) {
+                int cost = d[index] + graph.get(index).get(i).getDistance();
+                if (cost < d[graph.get(index).get(i).getIndex()]) {
+                    d[graph.get(index).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(index).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
 }
